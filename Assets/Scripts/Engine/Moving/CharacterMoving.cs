d1241c065daaf829df80533bd8f75a8a7cc897fd
Alex Gorbach <aleks.gorbach@gmail.com>
2015-08-26 21:31:39 +0300

    Moving controller, some decorations added

---
+++ Assets/Scripts/Engine/Moving/CharacterMoving.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Engine.Moving {
+
+    [RequireComponent(typeof(CharacterController))]
+    internal class CharacterMoving : HeroMovingController {
+        private CharacterController _controller;
+
+        protected override void Awake() {
+            base.Awake();
+            _controller = GetComponent<CharacterController>();
+        }
+
+        protected override void Run() {
+            _controller.SimpleMove(_runVector);
+        }
+
+        protected override void Jump() {;
+            _controller.Move(_jumpVector);
+        }
+    }
+}
