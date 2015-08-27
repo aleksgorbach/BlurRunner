822e7c4a621486ebb65018dc2fad882e86373434
Alex Gorbach <aleks.gorbach@gmail.com>
2015-08-27 20:44:29 +0300

    Test joints

--- Assets/Scripts/Gameplay/Heroes/Hero.cs
+++ Assets/Scripts/Gameplay/Heroes/Hero.cs
@@ -3,6 +3,11 @@
 namespace Assets.Scripts.Gameplay.Heroes {
     [RequireComponent(typeof(Animator))]
     internal class Hero : MonoBehaviour {
+        [SerializeField]
+        private Rigidbody2D _rigidbody;
+        [SerializeField]
+        private float _speed;
+
         private Animator _animator;
 
         private void Awake() {
@@ -10,7 +15,8 @@ namespace Assets.Scripts.Gameplay.Heroes {
         }
 
         public void StartMoving() {
-            _animator.SetTrigger("run_trigger");
+            //_animator.SetTrigger("run_trigger");
+            _rigidbody.AddForce(new Vector2(_speed, 0));
         }
     }
 }
