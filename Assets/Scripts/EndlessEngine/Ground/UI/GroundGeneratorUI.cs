3017484abdcde103d5f75efa7e1fbbb9dbe8289f
Alex Gorbach <aleks.gorbach@gmail.com>
2015-08-31 21:18:46 +0300

    GroundGeneratorUI

---
+++ Assets/Scripts/EndlessEngine/Ground/UI/GroundGeneratorUI.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Scripts.EndlessEngine.Ground.Generators;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.EndlessEngine.Ground.UI {
+    internal class GroundGeneratorUI : MonoBehaviour {
+        [Inject] private IGroundGenerator _generator;
+    }
+}
