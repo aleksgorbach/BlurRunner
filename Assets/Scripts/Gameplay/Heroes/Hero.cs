5700a5645dc0ec91edf3721e59c2bf42e66c6b56
aleksgorbach <aleks.gorbach@gmail.com>
2015-09-03 15:12:10 +0300

    background image size POT

--- Assets/Scripts/Gameplay/Heroes/Hero.cs
+++ Assets/Scripts/Gameplay/Heroes/Hero.cs
@@ -1,17 +1,5 @@
 ﻿using UnityEngine;
 
 namespace Assets.Scripts.Gameplay.Heroes {
-    internal class Hero : MonoBehaviour {
-        //[SerializeField] private Rigidbody2D _rigidbody;
-        //[SerializeField] private float _speed;
-        //[SerializeField] private float _jumpForce;
-
-        //private void Update() {
-        //    if (Input.touchCount > 0) {
-        //        _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Force);
-        //    }
-        //    _rigidbody.velocity = new Vector2(_speed, 0);
-        //}
-
-    }
+    internal class Hero : MonoBehaviour {}
 }
