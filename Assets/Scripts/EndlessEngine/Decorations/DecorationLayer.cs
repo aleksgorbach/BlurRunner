140ee9b9307b853b4a2ce83ca666f8193e278296
aleks.gorbach <aleks.gorbach@gmail.com>
2015-11-29 21:22:14 +0300

    Decorations separated into layers

---
+++ Assets/Scripts/EndlessEngine/Decorations/DecorationLayer.cs
@@ -0,0 +1,33 @@
+﻿// Created 28.11.2015
+// Modified by Александр 28.11.2015 at 22:05
+
+namespace Assets.Scripts.EndlessEngine.Decorations {
+    #region References
+
+    using Engine;
+    using Strategy;
+    using UnityEngine;
+    using Zenject;
+
+    #endregion
+
+    internal class DecorationLayer : AbstractGenerator<Decoration>, IDecorationGenerator {
+        [Inject]
+        private DecorationsFactory _factory;
+
+        [SerializeField]
+        private AbstractStrategy _strategy;
+
+        public override void Generate(float length, Decoration[] prefabs) {
+            _factory.Init(prefabs);
+            var currentPos = _strategy.DistanceToGenerate;
+            while (currentPos < length) {
+                var decoration = _factory.Create();
+                AddItem(decoration);
+                decoration.transform.SetParent(transform);
+                decoration.rectTransform.anchoredPosition3D = new Vector3(currentPos, 0, 0);
+                currentPos += _strategy.DistanceToGenerate;
+            }
+        }
+    }
+}
--- Assets/Scripts/EndlessEngine/Decorations/Decorations.cs
+++ Assets/Scripts/EndlessEngine/Decorations/Decorations.cs
@@ -1,32 +1,21 @@
-﻿// Created 03.11.2015
-// Modified by  27.11.2015 at 10:49
+﻿// Created 28.11.2015
+// Modified by Александр 28.11.2015 at 22:07
 
 namespace Assets.Scripts.EndlessEngine.Decorations {
     #region References
 
     using Engine;
-    using Strategy;
     using UnityEngine;
-    using Zenject;
 
     #endregion
 
     internal class Decorations : AbstractGenerator<Decoration>, IDecorationGenerator {
-        [Inject]
-        private DecorationsFactory _factory;
-
         [SerializeField]
-        private AbstractStrategy _strategy;
+        private DecorationLayer[] _layers;
 
         public override void Generate(float length, Decoration[] prefabs) {
-            _factory.Init(prefabs);
-            var currentPos = _strategy.DistanceToGenerate;
-            while (currentPos < length) {
-                var decoration = _factory.Create();
-                AddItem(decoration);
-                decoration.transform.SetParent(transform);
-                decoration.rectTransform.anchoredPosition3D = new Vector3(currentPos, 0, 0);
-                currentPos += _strategy.DistanceToGenerate;
+            foreach (var layer in _layers) {
+                layer.Generate(length, prefabs);
             }
         }
     }
