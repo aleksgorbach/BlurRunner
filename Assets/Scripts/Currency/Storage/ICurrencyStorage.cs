7f8d7f03341b1288ec857452f9547d6d5bf137f6
aleks.gorbach <aleks.gorbach@gmail.com>
2015-12-13 21:56:08 +0300

    Buggy graphics replaced

---
+++ Assets/Scripts/Currency/Storage/ICurrencyStorage.cs
@@ -0,0 +1,21 @@
+﻿// Created 13.12.2015
+// Modified by Александр 13.12.2015 at 20:03
+
+namespace Assets.Scripts.Currency.Storage {
+    #region References
+
+    using System;
+
+    #endregion
+
+    internal interface ICurrencyStorage {
+        int GetBalanse(CurrencyType currencyType = CurrencyType.Coins);
+
+        /// <summary>
+        /// Fired when not enough requested currency
+        /// </summary>
+        event Action<int, CurrencyType> NotEnoughRequestedCurrency;
+
+        bool Spend(int amount, CurrencyType currencyType = CurrencyType.Coins);
+    }
+}
